34c48055d260c17d1b35b4e4c639eed7b5d458b6
PamodN <145574206+PamodN@users.noreply.github.com>
2024-10-21 23:17:22 +0530

    loginapp

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "+1Tm/2TdrhkO+fx7O2PdW9WqLSFw6W32Mir1t11VuRk=",
+    EMAIL: "steve.franecki@ethereal.email", 
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI:"mongodb+srv://pamod1:pamod1@cluster0.w73nq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
